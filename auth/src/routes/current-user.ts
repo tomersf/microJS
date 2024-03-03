5ae0e6c3539c06ee243cd70731f16b426603398c
Tomer Shafir <tomer1up@gmail.com>
2024-03-03 17:44:12 +0200

    initial work on auth service

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', (req, res) => {
+    res.send('Test')
+})
+
+
+export { router as currentUserRouter }
