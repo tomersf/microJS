57de969344e236256d68803dece29f50f236517d
Tomer Shafir <tomer1up@gmail.com>
2024-03-04 14:22:41 +0200

    add user jwt

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
 
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
@@ -25,6 +26,15 @@ router.post('/api/users/signup', [
 
     const user = User.build({ email, password })
     await user.save()
+
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, 'supersecretkey')
+
+    req.session = {
+        jwt: userJwt
+    }
 
     res.status(201).send(user)
 })
