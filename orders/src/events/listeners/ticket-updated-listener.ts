fe3fc6429179579b4ccdd6d8a97f8c22d911fc03
Tomer Shafir <tomer1up@gmail.com>
2024-05-27 16:20:16 +0300

    add findByEvent method

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -4,27 +4,22 @@ import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
 
-
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
-    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
-    queueGroupName = queueGroupName;
-    async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-        const ticket = await Ticket.findOne({
-            _id: data.id,
-            version: data.version - 1
-        })
-        if (!ticket) {
-            throw new Error('Ticket not found')
-        }
-
-        const { title, price } = data
-        ticket.set({
-            title,
-            price
-        })
-        await ticket.save()
-
-        msg.ack()
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findByEvent(data);
+    if (!ticket) {
+      throw new Error("Ticket not found");
     }
 
+    const { title, price } = data;
+    ticket.set({
+      title,
+      price,
+    });
+    await ticket.save();
+
+    msg.ack();
+  }
 }
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -3,62 +3,75 @@ import { Order, OrderStatus } from "./order";
 import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface TicketAttrs {
-    title: string;
-    price: number;
-    id: string
+  title: string;
+  price: number;
+  id: string;
 }
 
 export interface TicketDoc extends mongoose.Document {
-    title: string;
-    price: number;
-    version: number;
-    isReserved(): Promise<boolean>;
+  title: string;
+  price: number;
+  version: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
-    build(attrs: TicketAttrs): TicketDoc;
+  build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
-const ticketSchema = new mongoose.Schema({
+const ticketSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true,
-        min: 0,
+      type: Number,
+      required: true,
+      min: 0,
     },
-}, {
+  },
+  {
     toJSON: {
-        transform(doc, ret) {
-            ret.id = ret._id;
-            delete ret._id;
-        }
-    }
-})
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
-ticketSchema.set('versionKey', 'version')
-ticketSchema.plugin(updateIfCurrentPlugin)
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-    const { id, ...rest } = attrs;
-    return new Ticket({
-        _id: id,
-        ...rest,
-    });
-}
+  const { id, ...rest } = attrs;
+  return new Ticket({
+    _id: id,
+    ...rest,
+  });
+};
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
 
 ticketSchema.methods.isReserved = async function () {
-    const existingOrder = await Order.findOne({
-        ticket: this,
-        status: {
-            $ne: OrderStatus.Cancelled
-        }
-    })
-    return !!existingOrder;
-}
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $ne: OrderStatus.Cancelled,
+    },
+  });
+  return !!existingOrder;
+};
 
-const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema)
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
 
-export { Ticket }
+export { Ticket };
